ca1ddc20ac58ed1408e2037b004c7d4ee8485f2a
imka239 <ima2ima3go9@gmail.com>
2019-04-28 21:36:07 +0300

    Add files via upload

--- expression.clj
+++ expression.clj
@@ -1,17 +1,17 @@
-(defn constant [value] (constantly value) )
+(def constant constantly)
 (defn variable [name] (fn [vars] (vars name)))
 
 (defn abstractOperation [func]
-  (fn [expr] (fn [vars] (func (expr vars))))
   (fn [& expr] (fn [vars] (apply func (mapv (fn [x] (x vars)) expr)))))
-(defn division [& expr]
-  (reduce (fn [a b] (/ a  (double b))) expr))
+(defn specOperation [func] (fn [& expr] (reduce func expr)))
 
 (def add (abstractOperation +))
 (def subtract (abstractOperation -))
 (def multiply (abstractOperation *))
 (def negate (abstractOperation -))
-(def divide (abstractOperation division))
+(def divide (abstractOperation (fn [a & b] (/ (double a)  (apply * b)))))
+(def min (abstractOperation (specOperation clojure.core/min)))
+(def max (abstractOperation (specOperation clojure.core/max)))
 
 (def operation
   {
@@ -20,12 +20,16 @@
    '* multiply
    '/ divide
    'negate negate
+   'min min
+   'max max
   })
 
-(defn parseFunc [expr] ( cond
-         (seq? expr) (apply (operation (first expr))(mapv parseFunc (rest expr)))
-         (number? expr) (constant expr)
-         :else (variable (str expr))
-     ))
+(defn parseFunc [expr]
+  (cond
+    (seq? expr) (apply (operation (first expr))(mapv parseFunc (rest expr)))
+    (number? expr) (constant expr)
+    :else (variable (str expr))))
+
 (def parseFunction
   (comp parseFunc read-string))
+
