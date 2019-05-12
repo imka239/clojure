f84f84fca30f1a1635d24c236badd8519be74778
imka239 <ima2ima3go9@gmail.com>
2019-05-12 22:56:19 +0300

    Add files via upload

--- linear.clj
+++ linear.clj
@@ -1,11 +1,9 @@
 (defn equals-len?
   ([] true)
-  ([v] true)
   ([vector1 & vectors]
-   (let [cnt (count vector1)] (every? (fn [x] (== (count x) cnt)) vectors)))
-  )
+   (let [cnt (count vector1)] (every? (fn [x] (== (count x) cnt)) vectors))))
 
-(defn scalar-vec? [& vals] (every? (fn [v] (number? v)) vals))
+(defn scalar-vec? [vec] (and (vector? vec) (every? number? vec)))
 
 (defn shape [a]
   (if (number? a)
@@ -24,40 +22,52 @@
        (mapv (operate-f func) v1 v2)))
     ))
 
+(defn broad? [t1 shapeT2] (= (nthrest shapeT2 (- (count shapeT2) (count (shape t1)))) (shape t1)))
 
-(defn broadcast [t1 t2]
-  {:pre (= (nthrest t2 (- (count t2) (count (shape t1)))) (shape t1))}
-  (if (equals-len? (shape t1) t2)
+(defn broadcast [t1 shapeT2]
+  (if (equals-len? (shape t1) shapeT2)
     t1
-    (vec (repeat (nth t2 0) (broadcast t1 (rest t2)))))
+    (vec (repeat (nth shapeT2 0) (broadcast t1 (rest shapeT2)))))
   )
 
 ;(m+ [[1 2 3] [4 5 6]] [[1 2 3][4 5 6]])
+
 (defn matrix? [m]
-  (and (vector? m) (apply equals-len? m)))
-;(every? (fn [x] (equals-len? (first m)  (first x))) mts)
-(defn checked-mat-op [op] (fn [m & mts]
-                            {:pre [(apply equals-len? m mts) (every? matrix? (apply vector m mts)) ]}
-                            (apply op m mts)))
+  (and (vector? m) (apply equals-len? m) (every? scalar-vec? m)))
 
+(defn tensor? [t] (or (number? t)
+                      (scalar-vec? t)
+                      (if (and (every? vector? t) (equals-len? t)) (tensor? ((fn [tens] (reduce (partial reduce conj) tens)) t)))))
+
+(defn check-matrix? [m & ms]
+  (and (apply equals-len? m ms) (every? matrix? (apply vector m ms))))
+
+(defn checked-mat-op [op] (fn [m & ms]
+                            {:pre [(and (apply check-matrix? m ms) (every? (fn [x] (equals-len? (first m) (first x))) ms))]}
+                            (apply op m ms)))
 (defn checked-vec-op [op] (fn [v & vcs]
-                            {:pre [(apply equals-len? v vcs)]}
+                            {:pre [(and (apply equals-len? v vcs) (scalar-vec? v) (every? scalar-vec? vcs))]}
                             (apply op v vcs)))
+(defn checked-tens-op [f] (fn
+                            ([t1] {:pre [(tensor? t1)]} (f t1))
+                            ([t1 t2]
+                             {:pre [(and (tensor? t1) (tensor? t2))]}
+                             (let [max-shape (max-key count (shape t1) (shape t2))]
+                               {:pre (and (broad? t1 max-shape) (broad? t2 max-shape))}
+                               (f (broadcast t1 max-shape) (broadcast t2 max-shape))))
+                            ([t1 t2 & t]
+                             {:pre (apply every? tensor? t1 t2 t)}
+                             (reduce (checked-tens-op f) ((checked-tens-op f) t1 t2) t))))
 
-(defn apply_v1_v2 [f] (checked-vec-op (fn [& vcs] (apply mapv f vcs))))
-(defn apply_m1_m2 [f] (checked-mat-op (fn [& vcs] (apply mapv f vcs))))
-(defn apply_t1_t2 [f]
-  (fn
-    ([t1] ((operate-f f) t1))
-    ([t1 t2]
-     (let [max-shape (max-key count (shape t1) (shape t2)) t1 (broadcast t1 max-shape) t2 (broadcast t2 max-shape)]
-       ((operate-f f) t1 t2)))
-    ([t1 t2 & t]
-     (reduce (apply_t1_t2 f) ((apply_t1_t2 f) t1 t2) t))))
+(defn apply_func [f] (partial mapv f))
 
+(defn apply_v1_v2 [f] (checked-vec-op (apply_func f)))
+(defn apply_m1_m2 [f] (checked-mat-op (apply_func f)))
+(defn apply_t1_t2 [f] (checked-tens-op (operate-f f)))
 (defn apply_v1_scalar [f] (fn
-                            ([arg] arg)
-                            ([arg & vals] (mapv (fn [layer] (apply f layer vals)) arg))))
+                            ([arg & vals] {:pre [(scalar-vec? arg) (every? number? vals)]} (mapv (fn [layer] (apply f layer vals)) arg))))
+(defn apply_m1_scalar [f] (fn
+                            ([arg & vals] {:pre [(matrix? arg) (every? number? vals)]} (mapv (fn [layer] (apply f layer vals)) arg))))
 
 (def v+ (apply_v1_v2 +))
 (def m+ (apply_m1_m2 v+))
@@ -66,30 +76,33 @@
 (def v* (apply_v1_v2 *))
 (def m* (apply_m1_m2 v*))
 (def v*s (apply_v1_scalar *))
-(def m*s (apply_v1_scalar v*s))
+(def m*s (apply_m1_scalar v*s))
 
 (defn scalar [& args] (apply + (apply v* args)))
 
 (defn vect
   ([v1 v2]
-   {:pre [(vector? v1) (vector? v2) (== (count v1) 3) (== (count v2) 3)]}
+   {:pre [(scalar-vec? v1) (scalar-vec? v2) (== (count v1) 3) (== (count v2) 3)]}
    (vector (- (* (nth v1 1) (nth v2 2)) (* (nth v1 2) (nth v2 1)))
            (- (* (nth v1 2) (nth v2 0)) (* (nth v1 0) (nth v2 2)))
            (- (* (nth v1 0) (nth v2 1)) (* (nth v1 1) (nth v2 0)))))
-  ([v1] {:pre [(vector? v1)]} v1)
+  ([v1] {:pre [(scalar-vec? v1)]} v1)
   ([v1 v2 & v]
    {:pre [(apply equals-len? v1 v2 v)]}
    (reduce vect (vect v1 v2) v))
   )
 
-(def m*v (fn [m v] (mapv(fn [row] (scalar row v)) m)))
-(defn transpose [m] (apply mapv vector m))
+(def m*v (fn [m v] {:pre [(matrix? m)]} (mapv (fn [row] (scalar row v)) m)))
+(defn transpose [m] {:pre [(matrix? m)]} (apply mapv vector m))
 (def m*m (fn
-           ([m1] m1)
-           ([m1 m2] (mapv (fn [ra] (mapv (fn [cb] (scalar ra cb)) (transpose m2))) m1))
-           ([m1 m2 & m] (reduce m*m (m*m m1 m2) m))
-           ))
+           ([m1] {:pre [(matrix? m1)]} m1)
+           ([m1 m2] {:pre [(and (matrix? m1) (matrix? m2)) (== (count (first m1)) (count (first (transpose m2))))]}
+            (mapv (fn [ra] (mapv (fn [cb] (scalar ra cb)) (transpose m2))) m1))
+           ([m1 m2 & m] (reduce m*m (m*m m1 m2) m))))
 
 (def b+ (apply_t1_t2 +))
 (def b* (apply_t1_t2 *))
 (def b- (apply_t1_t2 -))
+
+
+
